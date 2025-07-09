7256f945a1464f94c8627d94fff6fa718c6c1a43
Duy Long Nguyen <longlunglay1998@gmail.com>
2025-07-09 20:45:55 +1000

    feat: Add S3 bucket for pet media storage with lifecycle rules and CORS configuration

--- cdk/lib/shelter-hub.stack.ts
+++ cdk/lib/shelter-hub.stack.ts
@@ -2,12 +2,15 @@ import { Construct } from 'constructs';
 import { BaseStack } from './base-stack';
 import { PawfectMatchBaseStackProps } from './utils';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { RemovalPolicy, Duration } from 'aws-cdk-lib';
 
 export interface ShelterHubStackProps extends PawfectMatchBaseStackProps {}
 
 export class ShelterHubStack extends BaseStack {
   public readonly shelterAdminsTable: dynamodb.Table;
   public readonly sheltersTable: dynamodb.Table;
+  public readonly petMediaBucket: s3.Bucket;
 
   constructor(scope: Construct, id: string, props: ShelterHubStackProps) {
     super(scope, id, props);
@@ -45,5 +48,57 @@ export class ShelterHubStack extends BaseStack {
       },
       description: 'Table for storing pet information',
     });
+
+    // Create S3 Bucket for Pet Media (images and videos)
+    this.petMediaBucket = new s3.Bucket(this, `${this.stackName}-pet-media`, {
+      bucketName: `${this.stackName}-pet-media`.toLowerCase(),
+      versioned: true,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy:
+        this.environment === 'production'
+          ? RemovalPolicy.RETAIN
+          : RemovalPolicy.DESTROY,
+      lifecycleRules: [
+        {
+          id: 'delete-incomplete-multipart-uploads',
+          abortIncompleteMultipartUploadAfter: Duration.days(7),
+        },
+        {
+          id: 'transition-to-ia',
+          transitions: [
+            {
+              storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+              transitionAfter: Duration.days(30),
+            },
+            {
+              storageClass: s3.StorageClass.GLACIER,
+              transitionAfter: Duration.days(90),
+            },
+          ],
+        },
+      ],
+      cors: [
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+            s3.HttpMethods.DELETE,
+            s3.HttpMethods.HEAD,
+          ],
+          allowedOrigins: ['*'], // You might want to restrict this to your domain
+          allowedHeaders: ['*'],
+          exposedHeaders: ['ETag'],
+          maxAge: 3000,
+        },
+      ],
+    });
+
+    // Add tags to the S3 bucket
+    this.petMediaBucket.node.addMetadata(
+      'Description',
+      'S3 bucket for storing pet images and videos'
+    );
   }
 }
