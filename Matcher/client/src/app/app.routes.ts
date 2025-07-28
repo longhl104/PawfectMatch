4f4c27266ee36cefdc93b23914978622c08205d2
Duy Long Nguyen <longlunglay1998@gmail.com>
2025-07-28 23:12:30 +1000

    Remove home page component and related files, including README, HTML, SCSS, and TypeScript, to streamline the application structure and eliminate unused code.

--- Matcher/client/src/app/app.routes.ts
+++ Matcher/client/src/app/app.routes.ts
@@ -10,11 +10,6 @@ export const routes: Routes = [
       ),
   },
   {
-    path: 'home',
-    loadComponent: () =>
-      import('./pages/home/home.component').then((m) => m.HomeComponent),
-  },
-  {
     path: 'about',
     loadComponent: () =>
       import('./pages/about/about.component').then((m) => m.AboutComponent),
--- Matcher/client/src/app/components/footer/footer.component.ts
+++ Matcher/client/src/app/components/footer/footer.component.ts
@@ -3,19 +3,37 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DividerModule } from 'primeng/divider';
 import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 import { CustomIconComponent } from '@longhl104/pawfect-match-ng';
+import { CONTACT_INFO, CONTACT_ACTIONS, type ContactInfo } from '../../shared';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule, DividerModule, ButtonModule, CustomIconComponent],
+  imports: [
+    RouterModule,
+    DividerModule,
+    ButtonModule,
+    TooltipModule,
+    CustomIconComponent,
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  readonly contactInfo: ContactInfo = CONTACT_INFO;
   currentYear = new Date().getFullYear();
 
   openSocialLink(platform: string) {
-    console.log(`Opening ${platform} social link`);
+    switch (platform.toLowerCase()) {
+      case 'facebook':
+        CONTACT_ACTIONS.openUrl(this.contactInfo.facebook);
+        break;
+      case 'instagram':
+        CONTACT_ACTIONS.openUrl(this.contactInfo.instagram);
+        break;
+      default:
+        console.log(`Opening ${platform} social link`);
+    }
   }
 }
