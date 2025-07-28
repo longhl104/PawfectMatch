4dc2a42403156b8e7403be8a9cec0bd322f7852e
Duy Long Nguyen <longlunglay1998@gmail.com>
2025-07-28 22:34:43 +1000

    feat: Enhance footer component with custom icon and improved layout

--- Matcher/client/src/app/components/footer/footer.component.ts
+++ Matcher/client/src/app/components/footer/footer.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DividerModule } from 'primeng/divider';
 import { ButtonModule } from 'primeng/button';
+import { CustomIconComponent } from '@longhl104/pawfect-match-ng';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule, DividerModule, ButtonModule],
+  imports: [RouterModule, DividerModule, ButtonModule, CustomIconComponent],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
